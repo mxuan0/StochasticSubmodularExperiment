8318159b27d6cf0c8dc92b541e071b49534e20e4
mxuan0 <mxuan0@gmail.com>
2022-05-31 21:17:52 -0700

    colab update

--- SCG_NQP.py
+++ SCG_NQP.py
@@ -1,9 +1,8 @@
+from tabnanny import verbose
 import cvxpy as cp
 import numpy as np
 import pdb
-from YahooAdUtility import total_influence
 from tqdm import tqdm
-from copy import deepcopy
 
 class SCG_NQP:
     def __init__(self, H, A, h, u_bar, b) -> None:
@@ -18,13 +17,13 @@ class SCG_NQP:
         
         value = 1/2*x.T @ self.H @ x + self.h.T @ x
         gradient = self.H@x + self.h
-        
+        #pdb.set_trace()
         return value[0][0], gradient + noise
 
     def setup_constraints(self):
         self.x = cp.Variable(shape=(self.n, 1))
         self.p = cp.Parameter(shape=(self.n, 1))
-
+        
         constraints = [0 <= self.x, self.x <= self.u_bar, self.A @ self.x <= self.b]
         objective = cp.Maximize(self.x.T @ self.p)
 
@@ -32,7 +31,7 @@ class SCG_NQP:
 
     def project(self, momentum):
         self.p.value = momentum
-        self.prob.solve(solver='ECOS')
+        self.prob.solve()
         
         return self.x.value
 
@@ -40,48 +39,55 @@ class SCG_NQP:
         new_momentum = (1-p) * momentum + p * grad        
         grad_projected= self.project(new_momentum)
         
+        #return x + 1e-2 * grad_projected, new_momentum
         return x + 1/epoch * grad_projected, new_momentum
 
-    def train(self, epoch):
+    def train(self, epoch, c):
         x = np.zeros(shape=(self.n,1))
         momentum = np.zeros(shape=(self.n,1))
 
         values = []
+        momentum_grad_diff = []
         for e in range(epoch):
             #pdb.set_trace()
             p = 4 / (e+8)**(2/3)
+            #p = 1 / (e)**(2/3)
             value, grad = self.compute_value_grad(x)
-            x, momentum = self.stochastic_continuous_greedy_step(x, grad, p, momentum, e+1)
-
+            momentum_grad_diff.append(np.sum(np.square(grad-momentum)))
+            x, momentum = self.stochastic_continuous_greedy_step(x, grad, p, momentum, (e+10*c)/c)
+            
             values.append(value)
+            
 
-        return values 
+        return x, values, momentum_grad_diff
 
-n = 100
-m = 50
-b = 1
+# n = 100
+# m = 50
+# b = 1
 
-u_bar = np.ones((n,1))
-H = np.random.uniform(-100, 0, (n, n))
-A = np.random.uniform(0, 1, (m, n))
-h = -1 * H.T @ u_bar
+# u_bar = np.ones((n,1))
+# H = np.random.uniform(-100, 0, (n, n))
+# A = np.random.uniform(0, 1, (m, n))
+# h = -1 * H.T @ u_bar
 
-run = 1
-train_iter = 10000
+# run = 11
+# train_iter = 1000
 
-results = []
-for _ in tqdm(range(run)):
-    scg = SCG_NQP(H, A, h, u_bar, b)
-    values = scg.train(train_iter)
-    results.append(values[:])
+# for c in range(1, 102, 10):
+#     results = []
+#     for _ in tqdm(range(run)):
+#         scg = SCG_NQP(H, A, h, u_bar, b)
+#         x, values, momentum_grad_diff = scg.train(train_iter, c)
+#         pdb.set_trace()
+#         results.append(values[:])
 
-results = np.array(results)
+#     results = np.array(results)
 
-import matplotlib.pyplot as plt
-plt.figure()
-plt.plot(results.min(axis=0))
-# plt.plot(results.max(axis=0))
-# plt.plot(results.mean(axis=0))
-plt.plot(results.var(axis=0))
-plt.show()
-plt.savefig('Plots/SCG_NQP_b%d.png' % b)
+#     import matplotlib.pyplot as plt
+#     plt.figure()
+#     plt.plot(results.min(axis=0))
+#     plt.plot(results.max(axis=0))
+#     plt.plot(results.mean(axis=0)[:])
+#     #plt.plot(results.var(axis=0))
+#     #plt.show()
+#     plt.savefig('Plots/SCG_NQP_b1_c%d_10.png' % c)
